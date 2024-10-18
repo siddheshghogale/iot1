1b925161bf2cf7a7387724c4b478ddc4cd5515e6
siddheshghogale <sghoga99@gmail.com>
2024-10-18 14:50:00 +0530

    Update and rename nqueen.py to nqueentoh.py

--- nqueentoh.py
+++ nqueentoh.py
@@ -27,3 +27,14 @@ def issafe(board,row,col):
 board=[[0 for x in range(N)] for y in range(N)]
 if not snq(board,0):
     print("no solution found")
+
+
+
+
+def TOH(n,s,d,a):
+	if n==1:
+		print ("Move disk 1 from source",s,"to destination",d)
+		return
+	TOH(n-1,s,a,d)
+	print ("Move disk",n,"from source",s,"to destination",d)
+	TOH(n-1,a,d,s)
